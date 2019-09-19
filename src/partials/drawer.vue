add1f95bef4ffa5c6cc5d859426eac23d57c51cc
Charlie Thomason <thomason.charlie@gmail.com>
2019-09-19 15:57:36 -0500

    update homepage with experience, create drawer

---
+++ src/partials/drawer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="drawer">
+        <h2 class="drawer__heading">{{ heading }}</h2>
+        <div class="drawer__content">
+            <div class="drawer__main">
+                <slot name="main" />
+            </div>
+            <div class="drawer__more" :class="{ 'drawer__more--hidden': !expanded }">
+                <slot name="more" />
+            </div>
+        </div>
+        <button type="button" class="drawer__expand" @click.stop.prevent="toggleExpanded">{{ buttonText }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Drawer',
+    props: {
+        heading: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            expanded: false
+        };
+    },
+    computed: {
+        buttonText() {
+            return this.expanded ? 'View Less': 'View More';
+        }
+    },
+    methods: {
+        toggleExpanded() {
+            this.expanded = !this.expanded;
+        }
+    }
+};
+</script>
--- src/templates/index.vue
+++ src/templates/index.vue
@@ -7,22 +7,52 @@
                 <h1 class="h1">Charlie Thomason
                     <div class="subtitle">Artist and Software Engineer from Chicago</div>
                 </h1>
+                <drawer :heading="experience.heading">
+                    <template v-slot:main>
+                        <h3>{{ currentJob.company }}</h3>
+                        <div>{{ currentJob.position }} &mdash; {{ currentJob.location }}</div>
+                        <div>{{ currentJob.start }} &mdash; present</div>
+                        <ul>
+                            <li v-for="(note, i) in currentJob.notes" :key="`current-note-${i}`">
+                                {{ note }}
+                            </li>
+                        </ul>
+                    </template>
+                    <template v-slot:more>
+                        <div v-for="(job, i) in experience.past" :key="`past-job-${i}`">
+                            <h3>{{ job.company }}</h3>
+                            <div>{{ job.position }} &mdash; {{ job.location }}</div>
+                            <div>{{ job.start }} &mdash; {{ job.end }}</div>
+                            <ul>
+                                <li v-for="(note, j) in job.notes" :key="`past-job-${i}-note-${j}`">
+                                    {{ note }}
+                                </li>
+                            </ul>
+                        </div>
+                    </template>
+                </drawer>
             </div>
         </section>
     </main>
 </template>
 
 <script>
-import dataset from '../data/index.json';
+import { experience } from '../data/index.json';
 import MainHeader from '../partials/header.vue';
+import Drawer from '../partials/drawer.vue';
 
 export default {
     name: 'Homepage',
-    components: { MainHeader },
+    components: { MainHeader, Drawer },
     data() {
         return {
-            dataset
+            experience
         };
+    },
+    computed: {
+        currentJob() {
+            return experience.current;
+        }
     }
 };
 </script>
