a98bb51a22ec44c58954b8d8deb094a7514d30b9
Charlie Thomason <thomason.charlie@gmail.com>
2019-09-23 16:02:12 -0500

    refactored homepage to not need computed or methods

--- src/templates/index.vue
+++ src/templates/index.vue
@@ -4,31 +4,32 @@
         <section class="content">
             <div class="homepage">
                 <img src="~img/home/charlie-circle.png" class="charlie-circle" alt="Photo of Charlie Thomason" />
-                <h1 class="h1">Charlie Thomason
-                    <div class="subtitle">Artist and Software Engineer from Chicago</div>
+                <h1 class="h1">{{ homeData.heading }}
+                    <div class="subtitle">{{ homeData.subtitle }}</div>
                 </h1>
                 <div class="contact">
                     <icon-link
-                        v-for="(icon, i) in contact"
+                        v-for="(icon, i) in homeData.contact"
                         :key="i"
                         :label="icon.label"
                         :href="icon.href"
-                        :icon="getIcon(icon.icon)"
+                        :icon="icons[icon.icon]"
                     />
                 </div>
-                <drawer v-bind="experienceDrawer" />
-                <drawer v-bind="educationDrawer" />
+                <drawer v-bind="homeData.experience" />
+                <drawer v-bind="homeData.education" />
             </div>
         </section>
     </main>
 </template>
 
 <script>
-import { experience, education, contact } from '../data/index.json';
+import homeData from '../data/index.json';
+import icons from '../img/svg-icons/icons';
+
 import MainHeader from '../partials/header.vue';
 import Drawer from '../partials/drawer.vue';
 import IconLink from '../partials/icon-link.vue';
-import icons from '../img/svg-icons/icons';
 
 export default {
     name: 'Homepage',
@@ -39,26 +40,9 @@ export default {
     },
     data() {
         return {
-            experience,
-            education,
-            contact,
+            homeData,
             icons
         };
-    },
-    computed: {
-        experienceDrawer() {
-            const { heading, main, more } = this.experience;
-            return { heading, main, more };
-        },
-        educationDrawer() {
-            const { heading, main, more } = this.education;
-            return { heading, main, more };
-        }
-    },
-    methods: {
-        getIcon(icon) {
-            return this.icons[icon];
-        }
     }
 };
 </script>
