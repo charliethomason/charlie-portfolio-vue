aab682eaa259b6853020195d37ed1a777bf351ad
charliethomason <thomason.charlie@gmail.com>
2019-11-04 12:17:27 -0600

    fixed up galleries page

--- src/templates/galleries/galleries.vue
+++ src/templates/galleries/galleries.vue
@@ -4,13 +4,17 @@
             v-for="(gallery, i) in galleries" :key="i"
             tag="li"
             :to="gallery.href"
-            class="galleries-item"
+            class="galleries__item"
             exact-active-class="active"
             active-class="active"
         >
-            <a class="galleries-link">
-                <img src="https://via.placeholder.com/480" alt="" class="galleries-img" />
-                <span class="galleries-text">{{ gallery.name }}</span>
+            <a class="galleries__link">
+                <img
+                    :src="require('../../img/art/galleries/galleries-'+gallery.href+'-960.jpg')"
+                    :alt="`${gallery.name} gallery icon`"
+                    class="galleries__img"
+                />
+                <span class="galleries__text">{{ gallery.name }}</span>
             </a>
         </router-link>
     </ul>
