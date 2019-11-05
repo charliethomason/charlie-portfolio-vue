ebd1ee8adaafb620173abd694b6cf5a8eb975b48
charliethomason <thomason.charlie@gmail.com>
2019-11-05 15:48:56 -0600

    built out more code demo styles

--- src/partials/code-preview.vue
+++ src/partials/code-preview.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="code-preview">
-        <h2 class="code-preview__title">{{ title }}</h2>
+        <h2 class="code-preview__heading">
+            {{ title }}
+            <a :href="`/demos/${id}/`" class="code-preview__btn code-preview__btn--primary">View Demo</a>
+            <a :href="github" class="code-preview__btn code-preview__btn--secondary" target="_blank">View Code</a>
+            <span class="code-preview__btn code-preview__btn--year">{{ year }}</span>
+        </h2>
         <div class="code-preview__content">
+            <a :href="`/demos/${id}/`" class="code-preview__img">
+                <img :src="require('../img/code/'+id+'.jpg')" alt="title" />
+            </a>
             <div class="code-preview__info">
-                <div class="code-preview__year">{{ year }}</div>
                 <div class="code-preview__tags">{{ tagsList }}</div>
                 <p class="code-preview__description" v-html="description" />
-            </div>
-            <div class="code-preview__btns">
-                <a :href="`/demos/${id}/`">View Demo</a>
-                <a :href="github">View Code</a>
             </div>
         </div>
     </div>
