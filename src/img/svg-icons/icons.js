f30149c67378fae1d7056a5bb60e25da3615a0b4
charliethomason <thomason.charlie@gmail.com>
2020-11-06 17:12:09 -0600

    added youtube button

--- src/img/svg-icons/icons.js
+++ src/img/svg-icons/icons.js
@@ -1,11 +1,13 @@
 import ContactIcon from './contact.vue';
 import LinkedinIcon from './linkedin.vue';
 import InstagramIcon from './instagram.vue';
+import YouTubeIcon from './youtube.vue';
 import GithubIcon from './github.vue';
 
 export default {
     ContactIcon,
     LinkedinIcon,
     InstagramIcon,
+    YouTubeIcon,
     GithubIcon
 };
