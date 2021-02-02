aa0d207f38ed865878404f1fc1b136a5bfde767e
charliethomason <thomason.charlie@gmail.com>
2021-02-02 12:32:52 -0600

    add galleria to code section

--- src/partials/code-preview.vue
+++ src/partials/code-preview.vue
@@ -2,12 +2,12 @@
     <div class="code-preview">
         <h2 class="code-preview__heading">
             <span class="code-preview__heading__text">{{ title }}</span>
-            <a :href="`/demos/${id}/`" class="code-preview__btn code-preview__btn--primary">View Demo</a>
+            <a :href="demoUrl" class="code-preview__btn code-preview__btn--primary">View Demo</a>
             <a :href="github" class="code-preview__btn code-preview__btn--secondary" target="_blank">View Code</a>
             <span class="code-preview__btn code-preview__btn--year">{{ year }}</span>
         </h2>
         <div class="code-preview__content">
-            <a :href="`/demos/${id}/`" class="code-preview__img">
+            <a :href="demoUrl" class="code-preview__img">
                 <img :src="require('../img/code/'+id+'.jpg')" alt="title" />
             </a>
             <div class="code-preview__info">
@@ -35,6 +35,17 @@ export default {
         },
         github: {
             type: String
+        },
+        demo: {
+            type: String
+        }
+    },
+    computed: {
+        demoUrl() {
+            if (!!this.demo) {
+                return this.demo;
+            }
+            return `/demos/${this.id}/`;
         }
     }
 };
