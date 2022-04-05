0608fce2d62a4d19027e15da9093650f36b46be3
charliethomason <thomason.charlie@gmail.com>
2022-04-05 11:30:30 -0500

    updated homepage

--- src/partials/home-content.vue
+++ src/partials/home-content.vue
@@ -18,32 +18,27 @@
                 </ul>
             </div>
         </div>
-        <drawer heading="About Charlie">
-            My name is Charlie, and I love writing code and creating art. By day, I am a Staff UI Software Engineer at <a href="https://www.basis.net/" target="_blank">Basis Technologies</a>, specializing in front-end, design systems, and web accessibility. I have been a full-time web developer for over 9 years, and received my Master's degree from DePaul. By night, I am an Artist, specializing in ink, watercolor, and oil painting. I earned my BFA in Art Studio, and have self-published several books of my art, including <a href="https://www.blurb.com/b/7500748-birds-of-chicagoland" target="_blank"><em>Birds of Chicagoland</em></a>. In addition to being an engineer and artist, I am a guitarist for the Louisville, KY band <a href="https://www.youtube.com/channel/UC-SQ6raHnpW0yvpNibW6aRA" target="_blank">Echo's Aim</a>. In my free time, I enjoy <a href="https://letterboxd.com/charliefusion/" target="_blank">watching movies</a> and auto racing, <a href="https://www.goodreads.com/user/show/75584528-charlie-thomason" target="_blank">reading books</a>, running, and traveling. I live in Chicago with my wife, <a href="https://www.instagram.com/amanda_tea/" target="_blank">Amanda</a>, and cat, Baxter. This website is a portfolio of some of my best work.
-        </drawer>
-        <!-- <div class="big-btn-wrap">
-            <a
-                href="docs/CharlieThomason-Resume2022.pdf"
-                target="_blank"
-                class="big-btn"
-            >
-                Download Resume PDF
-            </a>
-        </div> -->
+        <section class="card">
+            <h2 class="card__heading">About Charlie</h2>
+            <div class="card__content">
+                My name is Charlie, and I love writing code and creating art. By day, I am a Staff UI Software Engineer at <a href="https://www.basis.net/" target="_blank">Basis Technologies</a>, specializing in front-end, design systems, and web accessibility. I have been a full-time web developer for over 9 years, and received my Master's degree from DePaul. By night, I am an Artist, specializing in ink, watercolor, and oil painting. I earned my BFA in Art Studio, and have self-published several books of my art, including <a href="https://www.blurb.com/b/7500748-birds-of-chicagoland" target="_blank"><em>Birds of Chicagoland</em></a>. In addition to being an engineer and artist, I am a guitarist for the Louisville, KY band <a href="https://www.youtube.com/channel/UC-SQ6raHnpW0yvpNibW6aRA" target="_blank">Echo's Aim</a>. In my free time, I enjoy <a href="https://letterboxd.com/charliefusion/" target="_blank">watching movies</a> and auto racing, <a href="https://www.goodreads.com/user/show/75584528-charlie-thomason" target="_blank">reading books</a>, running, and traveling. I live in the western Chicago suburbs with my wife, Amanda, and cat, Baxter. This website is a portfolio of some of my best work.
+            </div>
+        </section>
+        <footer-note />
     </div>
 </template>
 
 <script>
 import homeData from '../data/index.json';
 import icons from '../img/svg-icons/icons';
-import Drawer from './drawer.vue';
 import IconLink from './icon-link.vue';
+import FooterNote from './footer.vue';
 
 export default {
     name: 'HomeContent',
     components: {
-        Drawer,
-        IconLink
+        IconLink,
+        FooterNote
     },
     data() {
         return {
