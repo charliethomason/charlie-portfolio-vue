2befbe36fdb94bd0b5b40c3b9d3ebb891b21241f
Charlie Thomason <thomason.charlie@gmail.com>
2023-10-08 14:17:00 -0500

    removed multimedia page (changed my mind)

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,15 +1,13 @@
 import Homepage from '../components/pages/homepage.vue';
 import Galleries from '../components/pages/galleries.vue';
 import Collection from '../components/pages/collection.vue';
-import Multimedia from '../components/pages/multimedia.vue';
 
 const routes = [
 	{ path: '/', component: Homepage },
 	{ path: '/art', component: Galleries, meta: { id: 'art', title: 'Art' } },
 	{ path: '/art/:name', component: Collection, meta: { id: 'art', title: 'Art '} },
 	{ path: '/photo', component: Galleries, meta: { id: 'photo', title: 'Photo' } },
-	{ path: '/photo/:name', component: Collection, meta: { id: 'photo', title: 'Photo ' }  },
-	{ path: '/multimedia', component: Multimedia, meta: { id: 'multimedia', title: 'Multimedia' } }
+	{ path: '/photo/:name', component: Collection, meta: { id: 'photo', title: 'Photo ' }  }
 ];
 
 export default routes;
