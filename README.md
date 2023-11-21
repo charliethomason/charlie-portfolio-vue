e368822790b30cae29979bf02832cc5508d7dbad
charliethomason <thomason.charlie@gmail.com>
2023-11-20 18:53:29 -0600

    updated website url

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Charlie Thomason Portfolio Development Repo
 
-Development repo for the portfolio site of Charlie Thomason at [charliethomason.github.io](https://charliethomason.github.io). Built with Vue.js v2.6.
+Development repo for the portfolio site of Charlie Thomason at [charliethomason.com](https://charliethomason.com). Built with Vue.js v2.6.
--- src/components/elements/footer.vue
+++ src/components/elements/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="copyright">
-    All artwork on this website is property of <a href="https://charliethomason.github.io">Charlie Thomason</a>. Do not reuse or republish without permission.
+    All artwork on this website is property of <a href="http://charliethomason.com">Charlie Thomason</a>. Do not reuse or republish without permission.
   </div>
 </template>
 
