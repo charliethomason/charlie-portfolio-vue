ddecf9bdf12ab1d4d8a065e29bac884bee668189
charliethomason <thomason.charlie@gmail.com>
2025-07-20 02:41:24 -0500

    started adding theme toggle

--- src/components/elements/header.vue
+++ src/components/elements/header.vue
@@ -3,20 +3,32 @@
     <a href="/" class="header__logo" title="Charlie Thomason">
       <span class="sr-text">Charlie Thomason</span>
     </a>
+    <toggle :selected="darkTheme" @change="updateTheme" />
     <main-nav :active="active" />
   </header>
 </template>
 
 <script>
 import MainNav from "./nav.vue";
+import Toggle from "./toggle.vue";
 export default {
   name: "MainHeader",
-  components: { MainNav },
+  components: { MainNav, Toggle },
   props: {
     active: {
       type: String,
       default: "index",
-    },
+    }
   },
+  data() {
+    return {
+      darkTheme: true
+    };
+  },
+  methods: {
+    updateTheme(isDark) {
+      this.darkTheme = isDark;
+    }
+  }
 };
 </script>
---
+++ src/components/elements/toggle.vue
@@ -0,0 +1,28 @@
+<template>
+<fieldset class="toggle">
+  <label>
+    <input type="checkbox" :checked="selected" @change.stop="onChange" />
+    <span class="sr-text">Dark theme toggle</span>
+    <sun-icon v-if="!selected" />
+    <moon-icon v-if="selected" />
+  </label>
+</fieldset>
+</template>
+<script>
+import SunIcon from "../../img/svg-icons/sun.vue";
+import MoonIcon from "../../img/svg-icons/moon.vue";
+export default {
+  name: "Toggle",
+  components: { SunIcon, MoonIcon },
+  props: {
+    selected: {
+      type: Boolean
+    }
+  },
+  methods: {
+    onChange() {
+      this.$emit("change", !this.selected);
+    }
+  }
+}
+</script>
